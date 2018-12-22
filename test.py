c88c560f076f34fd7b4bc6aecd342d7a92e47932
Civils Catalyst <civilscatalyst@gmail.com>
2018-12-22 13:04:47 +0530

    braces added

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import random
 
 num = random.randint(1,6)
 num2 = random.randint(1,6)
-print 'dice number: ', num, num2
-print 'new line added'
+print ('dice number: ', num, num2)
+print ('new line added')
