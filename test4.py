d099d2f8685968b6081e8c32a4596691ca3c347a
Civils Catalyst <civilscatalyst@gmail.com>
2018-12-30 02:02:04 +0530

    changes made

--- test4.py
+++ test4.py
@@ -3,10 +3,11 @@
 
 from newspaper import Article
 
-url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+url = 'https://www.thehindu.com/news/national/norway-goes-green-in-delhi/article25860444.ece?homepage=true'
 
 article = Article(url)
 
 article.download()
 article.parse()
+print('The title is - ', article.title)
 print(article.text)
