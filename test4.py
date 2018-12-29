34f4912a6c9d688cb372481ca4b5578550922add
Civils Catalyst <civilscatalyst@gmail.com>
2018-12-30 01:47:58 +0530

    added scraping code

--- test4.py
+++ test4.py
@@ -1,2 +1,12 @@
 
-print 'Created 4th file'
+# Read more about usage on https://newspaper.readthedocs.io/en/latest/
+
+from newspaper import Article
+
+url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+
+article = Article(url)
+
+article.download()
+article.parse()
+print(article.text)
